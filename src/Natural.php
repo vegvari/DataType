75ea8cc38d90108d4a3422e136c3502611e4b950
Romeo Vegvari <romeo@vegvari.hu>
2015-01-19 22:25:09 +0000

    Rand removed from Natural

--- src/Natural.php
+++ src/Natural.php
@@ -17,18 +17,4 @@ class Natural extends Int
 
         return $value;
     }
-
-    /**
-     * Generate random
-     *
-     * @param  mixed $min
-     * @param  mixed $max
-     * @return mixed
-     */
-    public static function rand($min = 0, $max = null)
-    {
-        $min = Natural::cast($min);
-
-        return parent::rand($min, $max);
-    }
 }
