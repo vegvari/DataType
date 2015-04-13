4e67a7d99b4927f0c707af416d728fca0a0fe1d2
Romeo Vegvari <romeo@vegvari.hu>
2015-04-13 15:07:17 +0100

    Check2 removed

--- src/_float.php
+++ src/_float.php
@@ -43,25 +43,4 @@ class _float extends Number
 
         return (float) $value;
     }
-
-    public function check2($value)
-    {
-        if ($value === false || $value === 0 || $value === 0.0 || $value === '0') {
-            return 0.0;
-        } elseif ($value === true || $value === 1 || $value === 1.0 || $value === '1') {
-            return 1.0;
-        } elseif (is_float($value)) {
-            return $value;
-        } elseif ($value instanceof Float) {
-            return (float) $value->value();
-        } elseif ($value instanceof Basic) {
-            $value = $value->value();
-        }
-
-        if (filter_var($value, FILTER_VALIDATE_FLOAT) === false) {
-            throw new \InvalidArgumentException('Invalid float');
-        }
-
-        return (float) $value;
-    }
 }
